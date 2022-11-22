602dbe189d38b87a86eca59088dd984bd1056a08
Zsofia <111852105+sophsroom@users.noreply.github.com>
2022-11-22 13:12:54 +0000

    array of pictures

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,20 @@ import GridLayout from "react-grid-layout"
 import Button from "./components/Button"
 import Counter from  "./components/Counter.jsx"
 
+const tasks = [{
+  id: 1, // Used in JSX as a key
+  left: '/src/Xmas_tree.png',
+  right: '/src/Snowman.png',
+  correct: "left",
+},
+{
+  id: 2, // Used in JSX as a key
+  right: '/src/Xmas_tree.png',
+  left: '/src/Snowman.png',
+  correct: "right",
+}            
+             
+ ];
 
 export default function App() {
 const [count, setCount] = React.useState(0); 
@@ -16,11 +30,12 @@ const [slide, setSlide] = React.useState(1);
 <container style={{ display: 'flex', margin: 20 }}>
   
 <Button onClick={() => setCount(count + 1)} className="card" alt="Left">
-  <img src="/src/Xmas_tree.png" width={180} height={180}/>
+  <img src={tasks[slide -1] .left} width={180} height={180}/>
 </Button>
   
 <Button onClick={() => setCount(count)} className="card" alt="Right">
-  <img src="/src/Snowman.png" width={180} height={180}/>
+
+  <img src={tasks[slide -1] .right} width={180} height={180}/>
 </Button>
   
 </container>

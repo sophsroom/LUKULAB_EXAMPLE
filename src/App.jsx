25c9865de1c8bcb039651e3ee9a1c3cd350d6729
Zsofia <111852105+sophsroom@users.noreply.github.com>
2022-11-29 10:30:39 +0000

    audio connected to slide

--- src/App.jsx
+++ src/App.jsx
@@ -10,41 +10,42 @@ const tasks = [{
   left: '/1_1.jpg',
   right: '/1_2.jpg',
   correct: "left",
-  audio: '/mp3/1.mp3'
+  audio: '/mp3/1.mp3',
 },
 {
   id: 2, // Used in JSX as a key
   right: '/2_1.jpg',
   left: '/2_2.jpg',
   correct: "right",
-  audio: '/mp3/2.mp3'
+  audio: '/mp3/2.mp3',
 },  
 {
   id: 3, // Used in JSX as a key
   right: '/3_1.jpg',
   left: '/3_2.jpg',
   correct: "right",
+  audio: '/mp3/3.mp3',
 },  
 {
   id: 4, // Used in JSX as a key
   right: '/4_1.jpg',
   left: '/4_2.jpg',
   correct: "left",
-  audio: '/mp3/4.mp3'
+  audio: '/mp3/4.mp3',
 }, 
 {
   id: 5, // Used in JSX as a key
   right: '/5_1.jpg',
   left: '/5_2.jpg',
   correct: "right", 
-  audio: '/mp3/5.mp3'
+  audio: '/mp3/5.mp3',
 }, 
 {
   id: 6, // Used in JSX as a key
   right: '/6_1.jpg',
   left: '/6_2.jpg',
   correct: "left",
-  audio: '/mp3/6.mp3'
+  audio: '/mp3/6.mp3',
 },              
  ];
 
@@ -59,11 +60,11 @@ const [slide, setSlide] = React.useState(1);
 <container style={{ display: 'flex', margin: 20 }}>
   
 <Button onClick={() => setCount(count + 1)} className="card" alt="Left">
-  <img src={tasks[slide -1] .left} width={180} height={180}/>
+  <img src={tasks[slide -1] .left} width={220} height={180}/>
 </Button>
   
 <Button onClick={() => setCount(count)} className="card" alt="Right">
-  <img src={tasks[slide -1] .right} width={180} height={180}/>
+  <img src={tasks[slide -1] .right} width={220} height={180}/>
 </Button>
   
 </container>
@@ -77,12 +78,12 @@ const [slide, setSlide] = React.useState(1);
   <hr id="line"/>    
       
 <container>
-<Button onClick={() => setSlide(slide + 1)} action="next" > Next </Button>
+<Button onClick={() => setSlide(slide -1)} action="next" > Next </Button>
 </container> 
 <br></br> 
 
 
-    </main>
+</main>
     
   )
 }
